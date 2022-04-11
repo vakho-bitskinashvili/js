b7128d81b0047ce1494fe4e194a6f1c9f43743f2
vaxo <vaxobiwko31@gmail.com>
2022-04-11 10:36:38 +0400

    remove file

--- script1.js
+++
@@ -1,9 +0,0 @@
-function  sum(... arg){
-    let numberSum=0;
-    for (let item of arg){
-       numberSum=numberSum+item;
-       
-    }
-    console.log(numberSum);
-}
-sum(10, 50, 6, 7, 8, 11, 6, 3, 9);
