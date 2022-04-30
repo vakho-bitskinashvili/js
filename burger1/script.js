c9c9d89ccb271f0bf66f5580471682018b8ab683
vb <vaxobiwko31@gmail.com>
2022-04-30 13:41:47 +0400

    burgeri

---
+++ burger1/script.js
@@ -0,0 +1,8 @@
+let burgerButton = document.getElementById('toggleButton');
+let navBar = document.getElementById('navigation-ul');
+
+
+burgerButton.addEventListener('click', function() {
+    navBar.classList.toggle('toggle');
+    burgerButton.classList.toggle('active');
+})
